7d56322b3868d1ab832f4f4bbeab1b886cd452f9
Agustin586 <69434857+Agustin586@users.noreply.github.com>
2020-10-30 19:34:29 -0300

    Update RTC_3231.h

--- Include/RTC_3231.h
+++ Include/RTC_3231.h
@@ -1,12 +1,9 @@
+#ifndef _XTAL_FREQ
+#error Debe definir la frecuencia del oscilador
+#endif
+
 #ifndef XC_RTC3231_H
 #define XC_RTC3231_H
-#endif
-
-
-#ifndef XC_CONFIGURACION_H
-    #include "CONFIG_PIC.h"
-#endif
-
 
 _Bool mod_rtc=0,esc_rtc=0,val_rtc=0;
 char hora=0,min=0,seg=0,dia=0,mes=0,year=0,pos_rtc=1;
@@ -21,3 +18,4 @@ void RTC_send_fecha(void);
 void RTC_read_time(void);
 void RTC_read_date(void);
 
+#endif
